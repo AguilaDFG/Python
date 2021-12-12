f74887e0101396eb8bf379a276b64f6957220c24
aguiladfg <aguila.dfg@gmail.com>
2021-12-12 19:52:19 +0100

    day 21

--- Intermediate/020-021/snake_game/main.py
+++ Intermediate/020-021/snake_game/main.py
@@ -1,8 +1,12 @@
 from turtle import Screen
 import time
 from snake import Snake
+from food import Food
+from soreboard import Scoreboard
 snake = Snake()
+food = Food()
 screen = Screen()
+scoreboard = Scoreboard()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
@@ -17,4 +21,15 @@ while on:
     screen.update()
     time.sleep((0.1))
     snake.move()
+    if snake.head.distance(food) < 15:
+        food.go_to_ran()
+        scoreboard.add_score()
+        snake.expand()
+    if not -290<snake.head.xcor()<290 or not -290<snake.head.ycor()<290:
+        scoreboard.gameover()
+        on = False
+    for body in snake.snake_body[1:]:
+        if snake.head.distance(body) < 10:
+            scoreboard.gameover()
+            on = False
 screen.exitonclick()
--- Intermediate/020-021/snake_game/snake.py
+++ Intermediate/020-021/snake_game/snake.py
@@ -1,15 +1,16 @@
 from turtle import Turtle
 DISTANCE = 20
+START_POS = [(0,0), (-20,0), (-40,0)]
 class Snake:
     def __init__(self):
         self.snake_body = []
-        body0 = Turtle(shape="square")
-        body0.color("white")
-        body0.pu()
-        self.snake_body.append(body0)
+        for a in range(3):
+            body0 = Turtle(shape="square")
+            body0.color("white")
+            body0.pu()
+            body0.goto(START_POS[a])
+            self.snake_body.append(body0)
         self.head = self.snake_body[0]
-        self.expand()
-        self.expand()
     def move(self):
         for b in range(len(self.snake_body)-1, 0, -1):
             self.snake_body[b].goto(self.snake_body[b - 1].xcor(), self.snake_body[b - 1].ycor())
@@ -18,16 +19,8 @@ class Snake:
         body = Turtle(shape="square")
         body.color("white")
         body.pu()
-        if self.head.heading() == 0:
-            body.goto(self.head.xcor() + 20, self.head.ycor())
-        if self.head.heading() == 90:
-            body.goto(self.head.xcor(), self.head.ycor() + 20)
-        if self.head.heading() == 180:
-            body.goto(self.head.xcor() - 20, self.head.ycor())
-        if self.head.heading() == 270:
-            body.goto(self.head.xcor(), self.head.ycor() - 20)
-        self.snake_body.insert(0, body)
-        self.head = self.snake_body[0]
+        body.goto(self.snake_body[-1].position())
+        self.snake_body.append(body)
     def up(self):
         if not self.head.heading() == 270:
             self.head.setheading(90)
